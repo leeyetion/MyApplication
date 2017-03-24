2d645aa037d4f47a4d6869a2921020370db2a590
leeyetion@163.com <leeyetion@163.com>
2017-03-24 14:27:58 +0800

    commit

---
+++ app/src/main/java/phone/crc/com/myapplication/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package phone.crc.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
